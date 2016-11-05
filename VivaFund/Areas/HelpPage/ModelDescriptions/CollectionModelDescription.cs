0b86c25e8845b65334177e20aef8e24e8ba1dc40
konstantinos Kefalas <kons.kefalas@hotmail.com>
2016-11-05 22:06:45 +0200

    Add project files.

---
+++ VivaFund/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace VivaFund.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
